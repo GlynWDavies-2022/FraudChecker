5f713244d8680485c9b31d925db0876ddb9a0493
Glyn Davies <glynwdavies@gmail.com>
2026-06-04 19:05:38 +0100

    Added Worker project.

---
+++ FraudChecker.Worker/FraudCheckerWorker.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace FraudChecker.Worker;
+
+public class FraudCheckerWorker : BackgroundService
+{
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        return base.StartAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
